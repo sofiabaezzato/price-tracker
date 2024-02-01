debb0e022c59b681b515941ded4fc3d1cddd5834
sofiabaezzato <sofiabaezzato331@gmail.com>
2024-02-01 13:00:48 +0100

    chore: remove failonstatuscode

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
       baseUrl: 'http://localhost:3000',
-      testIsolation: false,
     },
     env: {
       test_email: "",
--- cypress/e2e/sign_in.cy.ts
+++ cypress/e2e/sign_in.cy.ts
@@ -4,9 +4,7 @@ describe(`Login and visit dashboard`, () => {
   it("navigate to the dashboard", () => {
     cy.initializeAuth();
     // open dashboard page
-    cy.visit(`/dashboard`,  {
-      failOnStatusCode: false,
-    });
+    cy.visit(`/dashboard`);
   
     cy.screenshot()
     // cy.get("h1").contains("Tracked products");   
