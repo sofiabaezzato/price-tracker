1adbab566b1a6c1c5149e9f58d5ae1c5c19b948d
sofiabaezzato <sofiabaezzato331@gmail.com>
2024-02-11 22:31:53 +0100

    refactor: split main test in smaller ones

--- cypress/e2e/signin_and_track_product.cy.ts
+++ cypress/e2e/signin_and_track_product.cy.ts
@@ -1,25 +1,24 @@
-describe(`Login and visit dashboard`, () => {
-  it("Navigate to the dashboard and add and remove a URL", () => {
+describe(`Login, visit dashboard and add/remove products`, () => {
+  beforeEach("Login and navigate to the dashboard", () => {
     cy.initializeAuth();
     // open dashboard page
     cy.visit('/dashboard', {
       failOnStatusCode: false,
       timeout: 30000
     });
+  });
 
+  it('Add a new tracked product and display the product card', () => {
     cy.get('[data-cy=urlInput]').type('https://www.amazon.it/echo-dot-con-orologio/dp/B09B8RVKGW')
-
     cy.get('[data-cy=submitBtn]').click()
     cy.get('[data-cy=submitBtn]').should('contain', 'Add...')
-
-    cy.get('[data-cy=productName]', { timeout: 10000}).should('contain', 'Echo Dot');   
-
+    cy.get('[data-cy=productName]', { timeout: 10000 }).should('contain', 'Echo Dot');   
     cy.screenshot()
+  })
 
-    cy.get('[data-cy=deleteBtn]').click()
-
+  it('Delete all tracked products and display the default message', () => {
+    cy.get('[data-cy=deleteBtn]').click({ multiple: true })
     cy.get('p').should('contain', 'Nothing to show here, yet!'); 
-
     cy.screenshot()
-  });
+  })
 });
